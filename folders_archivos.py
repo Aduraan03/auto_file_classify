697d7ad4cbdbecb2e3b07023c0d65c765fbe3dd8
Alejandro Duran de Huerta <adhm_03@hotmail.com>
2025-06-02 16:07:16 -0600

    Add files via upload

---
+++ folders_archivos.py
@@ -0,0 +1,8 @@
+class Folders:
+    # origin paths
+    download_folder = 'C:\\Users\Huixquilucan\\Downloads'
+    pdf_folder = 'C:\\Users\\Huixquilucan\\OneDrive\\Desktop\\PDFs'
+    text_folder = 'C:\\Users\\Huixquilucan\\OneDrive\\Desktop\\Words'
+    excel_folder = 'C:\\Users\\Huixquilucan\\OneDrive\\Desktop\\Excels'
+    image_folder = 'C:\\Users\\Huixquilucan\\OneDrive\\Desktop\\Images'
+    presentation_folder = 'C:\\Users\\Huixquilucan\\OneDrive\\Desktop\\Presentation'
